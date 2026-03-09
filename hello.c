44c72b3b291af57755b6f066074bcbccdb29043e
dhananjay21 <salavidhananjay21@gmail.com>
2026-03-10 00:26:32 +0530

    added new file math.h

--- hello.c
+++ hello.c
@@ -1,9 +1,15 @@
-
+#include "math.h"
 #include <stdio.h>
 
 int main()
 {
 	// print Hello program
 	printf("Hello\n");
+
+	int sum = add(5,6);
+	int rem = sub(52,45);
+
+	printf("Adding 5 and 6 = %d\nSubtracting 45 from 52= %d\n", sum, rem);
+
 	return 0;
 }
